1adf90b05b8408683af955645a2251981b378437
natabask <natabask@yandex.ru>
2022-08-05 17:41:06 +0300

    build.properties added, screenshot changed in Report.md

--- src/test/java/ru/netology/data/DBHelper.java
+++ src/test/java/ru/netology/data/DBHelper.java
@@ -15,7 +15,7 @@ public class DBHelper {
         var runner = new QueryRunner();
         try (
                 Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                        "jdbc:mysql://localhost:3306/app", System.getProperty("user"), System.getProperty("password")
                 );
         ) {
             String paymentStatus = runner.query(conn, usersSQL, new ScalarHandler<>());
@@ -29,7 +29,7 @@ public class DBHelper {
         var runner = new QueryRunner();
         try (
                 Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                        "jdbc:mysql://localhost:3306/app", System.getProperty("user"), System.getProperty("password")
                 );
         ) {
             String creditStatus = runner.query(conn, usersSQL, new ScalarHandler<>());
